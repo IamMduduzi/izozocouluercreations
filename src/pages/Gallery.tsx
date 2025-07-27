b912059315fc971327fd8626bc010779bf3c63d2
Mduduzi <104123934+IamMduduzi@users.noreply.github.com>
2025-07-27 09:35:56 +0200

    Update Gallery.tsx

--- src/pages/Gallery.tsx
+++ src/pages/Gallery.tsx
@@ -5,7 +5,7 @@ import { Card } from "@/components/ui/card";
 import weddingImage from "@/assets/wedding-category.jpg";
 import graduationImage from "@/assets/graduation-category.jpg";
 import matricImage from "@/assets/matric-dance-category.jpg";
-import matrciImage from "@/assets/matrci-dance-category.jpg";
+import matrciImage from "@/assets/matrci-dance.jpg";
 import traditionalImage from "@/assets/traditional-category.jpg";
 import traditionalImage from "@/assets/traditional-category.jpg";
 
